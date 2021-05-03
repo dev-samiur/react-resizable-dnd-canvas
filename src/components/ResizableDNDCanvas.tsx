3fb8db7962149bd4aabe3852e564f0fe6b1fa11c
Samiur <samiurrahman.dev@gmail.com>
2021-05-03 18:56:22 +0600

    feat: add dnd component

--- src/components/ResizableDNDCanvas.tsx
+++ src/components/ResizableDNDCanvas.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Rnd } from "react-rnd";
+import { Box } from '@material-ui/core';
 
-const ResizableDNDCanvas= () => {
-  return (
-    <div>
+interface ResizableDNDCanvasProps{
+    imgSrc: string
+    // handleHeightChange(e:number):void
+    // handleWidthChange(e:number):void
+}
 
-    </div>
+const style = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "solid 1px #ddd",
+    background: "#f0f0f0",
+    overflow: 'hidden'
+};
+
+const ResizableDNDCanvas: React.FC<ResizableDNDCanvasProps>= ({imgSrc}) => {
+  
+ const [x, setX]= useState<number>(10) 
+ const [y, setY]= useState<number>(10) 
+ const [width, setWidth]= useState<string>('400') 
+ const [height, setHeight]= useState<string>('400') 
+
+ return (
+    <Rnd
+        style={style}
+        size={{ width: width, height: height }}
+        position={{ x: x, y: y }}
+        onDragStop={(e, d) => {
+          setX(d.x)
+          setY(d.y)
+        }}
+        onResizeStop={(e, direction, ref, delta, position) => {
+            setWidth(ref.style.width)
+            setHeight(ref.style.height)
+            setX(position.x)
+            setY(position.y)
+          }
+        }
+      >
+        <img src={imgSrc} style={{width: width, height: height}} />
+      </Rnd>
   );
 }
 
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import Layout from '../components/Layout'
 import { Container, Grid } from '@material-ui/core';
+import ResizableDNDCanvas from '../components/ResizableDNDCanvas'
 
 const Home= () => {
-  return (
-    <div>
-      <Layout />
-      <Container style={{paddingTop: 50}}>
-          <Grid container justify="center" spacing={5}>
-              <Grid item xs={12} md={6} style={{border: '1px solid red'}}>
-                    Row 1
-              </Grid>
-              <Grid item xs={12} md={6} style={{border: '1px solid red'}}>
+
+    const handleWidthChange= () => {
+
+    }
+
+    const handleHeightChange= () => {
+        
+    }
+
+    return (
+    <>
+        <Layout />
+        <Container style={{paddingTop: 50}}>
+            <Grid container justify="center" spacing={5}>
+                <Grid 
+                    item 
+                    xs={12} md={7} 
+                    style={{overflow: 'hidden', height: 600, border: '1px solid red', position: 'relative'}}
+                >
+                    <ResizableDNDCanvas 
+                        imgSrc="https://digitalsynopsis.com/wp-content/uploads/2017/06/beautiful-color-palettes-combinations-schemes-moonlight-bytes-6.png"
+                        // handleHeightChange={handleHeightChange} 
+                        // handleWidthChange={handleWidthChange}
+                    />
+                </Grid>
+                <Grid item xs={12} md={5} style={{border: '1px solid red'}}>
                     Row 2
-              </Grid>
-          </Grid>
-      </Container>
-    </div>
-  );
+                </Grid>
+                <Grid item>
+                    
+                </Grid>
+            </Grid>
+        </Container>
+    </>
+    );
 }
 
 export default Home;
